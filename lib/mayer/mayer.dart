a4094d479a315c61a0748afcf859e50d4163fba4
Seppi Hr. Chef <seppihd@hotmail.com>
2021-08-21 16:51:10 +0200

    Dummy page for later

---
+++ lib/mayer/mayer.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class MayerPage extends StatefulWidget {
+  const MayerPage({ Key? key }) : super(key: key);
+
+  @override
+  _MayerPageState createState() => _MayerPageState();
+}
+
+class _MayerPageState extends State<MayerPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Container(),
+    );
+  }
+}
