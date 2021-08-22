ba3f647aa3780a213189ee8e56f43de009566cdc
Seppi Hr. Chef <seppihd@hotmail.com>
2021-08-22 10:25:01 +0200

    Mayer button widget

--- lib/mayer/mayer.dart
+++ lib/mayer/mayer.dart
@@ -11,7 +11,27 @@ class _MayerPageState extends State<MayerPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Container(),
+
+  Widget _mayerOutlinedButton({String topText = "", String bottomText = "", required GestureTapCallback onPressed}){
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 5),
+      child: OutlinedButton(
+        style: ButtonStyle(
+          minimumSize: MaterialStateProperty.all<Size>(Size(double.infinity, 75)),
+          backgroundColor: MaterialStateProperty.all<Color>(Theme.of(context).primaryColor),
+          foregroundColor: MaterialStateProperty.all<Color>(Colors.white),
+          side: MaterialStateProperty.all<BorderSide>(BorderSide(color: Theme.of(context).accentColor, width: 4)),
+          textStyle: MaterialStateProperty.all<TextStyle>(TextStyle(fontSize: 20)),
+          
+        ),
+        child: SafeArea(
+          child: ListTile(
+            title: Text(topText, style: TextStyle(color: Colors.white)),
+            subtitle: Text(bottomText, style: TextStyle(color: Colors.grey))
+          ),
+        ),
+        onPressed: onPressed,
+      ),
     );
   }
 }
